88de96d8d091674c1afc8c4470c775156ac9c1c7
Raif Zeidam <mrzeidam2@gmail.com>
2022-04-18 13:30:56 -0300

    projeto criado e feitas algumas configuracoes

---
+++ src/main/java/com/meuprojeto/projetospringbooth2/ProjetoSpringbootH2Application.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.projetospringbooth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/meuprojeto/projetospringbooth2/ProjetoSpringbootH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.projetospringbooth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpringbootH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
