4e26d36c8221336253073e9cff73e7c1acd0cd96
Raif Zeidam <mrzeidam2@gmail.com>
2022-04-18 15:42:47 -0300

    criado orderstatus

--- src/main/java/com/meuprojeto/projetospringbooth2/config/Config.java
+++ src/main/java/com/meuprojeto/projetospringbooth2/config/Config.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.meuprojeto.projetospringbooth2.entities.Order;
 import com.meuprojeto.projetospringbooth2.entities.User;
+import com.meuprojeto.projetospringbooth2.entities.enums.OrderStatus;
 import com.meuprojeto.projetospringbooth2.repositories.OrderRepository;
 import com.meuprojeto.projetospringbooth2.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class Config implements CommandLineRunner {
 		User u1 = new User(null, "Maria Silva", "maria@gmail.com", "999999999", "12345");
 		User u2 = new User(null, "Jose Silva", "jose@gmail.com", "888888888", "12345");
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
